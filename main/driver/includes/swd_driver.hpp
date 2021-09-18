bea858873918b53e57ad051f9e2d031da68d1b8c
Jackson Ming Hu <huming2207@gmail.com>
2021-09-18 14:07:02 +1000

    Move daplink to submodule

--- main/driver/includes/swd_driver.hpp
+++ main/driver/includes/swd_driver.hpp
@@ -26,11 +26,16 @@ private:
 private:
     swd_driver() = default;
     SWD_FAST_FUNC_ATTR void send_bits(uint32_t bits, size_t bit_len);
-    SWD_FAST_FUNC_ATTR void send_cycles(uint8_t clk_cycles);
+    SWD_FAST_FUNC_ATTR void send_cycles(uint8_t clk_cycles, uint32_t swclk_level = 0xffffffff);
     SWD_FAST_FUNC_ATTR esp_err_t wait_till_ready(int32_t timeout_cycles);
+    IRAM_ATTR esp_err_t recv_pkt(uint8_t reg, uint32_t *out_word, uint16_t retry_cnt = 1000);
+    SWD_FAST_FUNC_ATTR esp_err_t read_dp(uint8_t reg, uint32_t *out_word);
+    static SWD_FAST_FUNC_ATTR uint8_t calc_parity_req(uint8_t reg);
+    static SWD_FAST_FUNC_ATTR uint32_t calc_parity_32(uint32_t reg);
 
 public:
-    esp_err_t init(gpio_num_t swclk = GPIO_NUM_1, gpio_num_t swdio = GPIO_NUM_2, uint32_t freq_hz = 10000000, spi_host_device_t host = SPI3_HOST);
-    void send_swj_sequence();
+    esp_err_t init(gpio_num_t swclk = GPIO_NUM_12, gpio_num_t swdio = GPIO_NUM_11, uint32_t freq_hz = 5000000, spi_host_device_t host = SPI2_HOST);
+    void IRAM_ATTR send_swj_sequence();
+    esp_err_t IRAM_ATTR read_idcode(uint32_t *idcode);
 };
 
