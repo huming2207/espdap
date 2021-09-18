6b8cbe4421351aee675c3b7cf28161136edc1227
Jackson Ming Hu <huming2207@gmail.com>
2021-09-18 16:46:55 +1000

    flash_algo: add some error checks

--- main/prog/flash_algo.cpp
+++ main/prog/flash_algo.cpp
@@ -36,13 +36,33 @@ esp_err_t flash_algo::parse()
     idcode = mpack_node_u32(mpack_node_map_cstr(root, "idcode"));
     pc_init = mpack_node_u32(mpack_node_map_cstr(root, "pc_init"));
     pc_uninit = mpack_node_u32(mpack_node_map_cstr(root, "pc_uninit"));
+    if (pc_init == 0 || pc_uninit == 0) {
+        ESP_LOGE(TAG, "No init/uninit function found in the algorithm!");
+        return ESP_ERR_NOT_FOUND;
+    }
+
     pc_program_page = mpack_node_u32(mpack_node_map_cstr(root, "pc_program_page"));
+    if (pc_program_page == 0) {
+        ESP_LOGE(TAG, "No program page function found in the algorithm!");
+        return ESP_ERR_NOT_FOUND;
+    }
+
     pc_erase_sector = mpack_node_u32(mpack_node_map_cstr(root, "pc_erase_sector"));
+    if (pc_program_page == 0) {
+        ESP_LOGE(TAG, "No erase sector function found in the algorithm!");
+        return ESP_ERR_NOT_FOUND;
+    }
+
     pc_erase_all = mpack_node_u32(mpack_node_map_cstr(root, "pc_erase_all"));
     ram_size_byte = mpack_node_u32(mpack_node_map_cstr(root, "ram_size"));
     flash_size_byte = mpack_node_u32(mpack_node_map_cstr(root, "flash_size_byte"));
     flash_start_addr = mpack_node_u32(mpack_node_map_cstr(root, "flash_start_addr"));
     flash_end_addr = mpack_node_u32(mpack_node_map_cstr(root, "flash_end_addr"));
+    if (ram_size_byte == 0 || flash_size_byte == 0 || flash_start_addr == 0 || flash_end_addr == 0) {
+        ESP_LOGE(TAG, "Flash details not sufficient");
+        return ESP_ERR_NOT_FOUND;
+    }
+
     page_size = mpack_node_u32(mpack_node_map_cstr(root, "page_size"));
     erased_byte_val = mpack_node_u32(mpack_node_map_cstr(root, "erased_byte_val"));
     program_page_timeout = mpack_node_u32(mpack_node_map_cstr(root, "program_page_timeout"));
@@ -55,7 +75,7 @@ esp_err_t flash_algo::parse()
 
     if (target_name_len < 1 || target_name_len > 255) {
         ESP_LOGE(TAG, "No target name given, or too long");
-        return ESP_ERR_INVALID_RESPONSE;
+        return ESP_ERR_NOT_FOUND;
     }
 
     target_name = new char[target_name_len + 1];
@@ -69,12 +89,11 @@ esp_err_t flash_algo::parse()
 
     if (algo_name_len < 1 || algo_name_len > 255) {
         ESP_LOGE(TAG, "No algorithm name given, or too long: ");
-        return ESP_ERR_INVALID_RESPONSE;
+        return ESP_ERR_NOT_FOUND;
     }
 
     algo_name = new char[algo_name_len + 1];
     memset(algo_name, 0, algo_name_len + 1);
-
     mpack_node_copy_cstr(algo_name_node, algo_name, algo_name_len);
 
     return ESP_OK;
