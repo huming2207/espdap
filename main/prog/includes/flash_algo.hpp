f6f50d6b5c3945ecfe5fa3d58e48a8fd15fe9982
Jackson Ming Hu <huming2207@gmail.com>
2021-09-18 17:33:50 +1000

    flash_algo: generate getters

--- main/prog/includes/flash_algo.hpp
+++ main/prog/includes/flash_algo.hpp
@@ -19,15 +19,32 @@ public:
     flash_algo() = default;
     ~flash_algo();
     esp_err_t init(const uint8_t *mp_file, size_t len);
-    esp_err_t parse();
+    char *get_algo_name() const;
+    char *get_target_name() const;
+    uint8_t *get_algo_bin() const;
+    uint32_t get_algo_bin_len() const;
+    uint32_t get_ram_size_byte() const;
+    uint32_t get_flash_size_byte() const;
+    uint32_t get_pc_init() const;
+    uint32_t get_pc_uninit() const;
+    uint32_t get_pc_program_page() const;
+    uint32_t get_pc_erase_sector() const;
+    uint32_t get_pc_erase_all() const;
+    uint32_t get_data_section_offset() const;
+    uint32_t get_flash_start_addr() const;
+    uint32_t get_flash_end_addr() const;
+    uint32_t get_page_size() const;
+    uint32_t get_erased_byte_val() const;
+    uint32_t get_program_page_timeout() const;
+    uint32_t get_erase_sector_timeout() const;
+    const sector_info &get_sector() const;
 
 private:
-    uint8_t sector_info_cnt = 0;
-    uint8_t *mp_buf = nullptr;
     static const constexpr char *TAG = "flash_algo";
     char *algo_name = nullptr;
     char *target_name = nullptr;
-    uint32_t idcode = 0;
+    uint8_t *algo_bin = nullptr;
+    uint32_t algo_bin_len = 0;
     uint32_t ram_size_byte = 0;
     uint32_t flash_size_byte = 0;
     uint32_t pc_init = 0;
@@ -42,7 +59,7 @@ private:
     uint32_t erased_byte_val = 0;
     uint32_t program_page_timeout = 0;
     uint32_t erase_sector_timeout = 0;
-    sector_info sector_info_arr[8] = {};
+    sector_info sector = {};
 
     mpack_tree_t mp_tree = {};
 };
