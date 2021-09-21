61e47fac03cca929ad5bb740af05fb00ddbe8e5a
Jackson Ming Hu <huming2207@gmail.com>
2021-09-21 22:27:34 +1000

    manifest_mgr: move to JSON

--- main/prog/includes/manifest_mgr.hpp
+++ main/prog/includes/manifest_mgr.hpp
@@ -2,10 +2,13 @@
 
 #include <esp_err.h>
 #include <mpack.h>
+#include <ArduinoJson.hpp>
+
+#define MANIFEST_SIZE_BYTES 3072
+#define MANIFEST_SIZE_JSON_DOC 4096
 
 struct manifest_info
 {
-    uint32_t idcode;
     uint32_t algo_checksum;
     uint32_t fw_checksum;
     const char *algo_name;
@@ -26,17 +29,19 @@ public:
 
 private:
     manifest_mgr() = default;
-    esp_err_t parse_manifest_item(mpack_node_t node, size_t idx);
+    esp_err_t parse_manifest_item(ArduinoJson::JsonVariantConst item);
 
 
 private:
-    static const constexpr char *TAG = "manifest";
+    static const constexpr char *TAG = "manifest_mgr";
     static const constexpr char *BASE_PATH = "/soul";
-    static const constexpr char *MANIFEST_PATH = "/soul/manifest.bin";
+    static const constexpr char *MANIFEST_PATH = "/soul/manifest.json";
 
 private:
     uint8_t manifest_cnt = 0;
     manifest_info manifests[8] = {};
+    char *manifest_buf = new char[MANIFEST_SIZE_BYTES];
+    ArduinoJson::DynamicJsonDocument json_doc = ArduinoJson::DynamicJsonDocument(MANIFEST_SIZE_JSON_DOC);
 
 public:
     esp_err_t init();
--- main/prog/manifest_mgr.cpp
+++ main/prog/manifest_mgr.cpp
@@ -1,8 +1,10 @@
 #include <esp_spiffs.h>
 #include <esp_log.h>
 #include <mpack.h>
+#include <ArduinoJson.hpp>
 #include "manifest_mgr.hpp"
 
+using namespace ArduinoJson;
 
 esp_err_t manifest_mgr::init()
 {
@@ -18,82 +20,74 @@ esp_err_t manifest_mgr::init()
         return ret;
     }
 
-    mpack_tree_t tree = {};
-    mpack_tree_init_filename(&tree, MANIFEST_PATH, 3072);
-    if (mpack_tree_error(&tree) != mpack_ok) {
-        ESP_LOGE(TAG, "Msgpack node tree failed to init");
-        mpack_tree_destroy(&tree);
-        return ESP_ERR_NO_MEM;
-    }
-
-    mpack_tree_parse(&tree);
-    mpack_node_t root = mpack_tree_root(&tree);
-
-    auto root_arr_len = mpack_node_array_length(root);
-    if (root_arr_len == 0) {
-        ESP_LOGE(TAG, "Nothing provided in the manifest");
-        mpack_tree_destroy(&tree);
+    FILE *file = fopen(MANIFEST_PATH, "r");
+    if (file == nullptr) {
+        ESP_LOGE(TAG, "Failed when reading file");
         return ESP_ERR_NOT_FOUND;
     }
 
-    for (size_t idx = 0; idx < root_arr_len; idx += 1) {
-        ret = parse_manifest_item(root, idx);
-        if (ret != ESP_OK) {
-            ESP_LOGE(TAG, "Failed when decoding manifest @ idx = %u", idx);
-            mpack_tree_destroy(&tree);
-            return ret;
-        }
+    fseek(file, 0, SEEK_END);
+    size_t len = ftell(file);
+    if (len < 1 || len > MANIFEST_SIZE_BYTES) {
+        ESP_LOGE(TAG, "Manifest in a wrong length: %u", len);
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    fseek(file, 0, SEEK_SET);
+    fread(manifest_buf, 1, len, file);
+    fclose(file);
+
+    if (deserializeJson(json_doc, manifest_buf, MANIFEST_SIZE_BYTES) != DeserializationError::Ok) {
+        ESP_LOGE(TAG, "Failed to parse manifest JSON");
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    auto root_array = json_doc.as<JsonArrayConst>();
+    for(auto item : root_array) {
+        ret = parse_manifest_item(item);
     }
 
     return ret;
 }
 
-esp_err_t manifest_mgr::parse_manifest_item(mpack_node_t node, size_t idx)
+esp_err_t manifest_mgr::parse_manifest_item(JsonVariantConst item)
 {
     if (manifest_cnt >= (sizeof(manifests) / sizeof(manifest_info))) {
         ESP_LOGE(TAG, "Manifest list is full!");
         return ESP_ERR_NO_MEM;
     }
 
-    auto item_node = mpack_node_array_at(node, idx);
-    auto fw_checksum = mpack_node_u32(mpack_node_map_cstr(item_node, "fw_crc"));
-    auto alg_checksum = mpack_node_u32(mpack_node_map_cstr(item_node, "alg_crc"));
+    if (item.isNull() || item.isUndefined()) {
+        ESP_LOGW(TAG, "Empty manifest entry found!");
+        return ESP_ERR_NOT_FOUND;
+    }
+
+
+    auto fw_checksum = item["fwCrc"].as<uint32_t>();
+    auto alg_checksum = item["algCrc"].as<uint32_t>();
     if (alg_checksum == 0 || fw_checksum == 0) {
         ESP_LOGE(TAG, "Checksum info not found");
         return ESP_ERR_NOT_FOUND;
     }
 
-    auto idcode = mpack_node_u32(mpack_node_map_cstr(item_node, "idcode"));
-    if (idcode == 0) {
-        ESP_LOGE(TAG, "IDCODE not found");
-        return ESP_ERR_NOT_FOUND;
-    }
-
-    auto fw_name_node = mpack_node_map_cstr(item_node, "fw_name");
-    auto fw_name_len = mpack_node_strlen(fw_name_node);
+    const char *fw_name = item["fwName"].as<const char *>();
+    size_t fw_name_len = strlen(fw_name);
     if (fw_name_len < 1 || fw_name_len > 127) {
         ESP_LOGE(TAG, "Invalid firmware file name size: %u", fw_name_len);
         return ESP_ERR_INVALID_SIZE;
     }
 
-    char fw_name[128] = { 0 };
-    mpack_node_copy_cstr(fw_name_node, fw_name, sizeof(fw_name));
-
-    auto alg_name_node = mpack_node_map_cstr(item_node, "alg_name");
-    auto alg_name_len = mpack_node_strlen(alg_name_node);
-    if (alg_name_len < 1 || alg_name_len > 127) {
-        ESP_LOGE(TAG, "Invalid flash algorithm file name size: %u", alg_name_len);
+    const char *alg_name = item["fwName"].as<const char *>();
+    size_t alg_name_len = strlen(fw_name);
+    if (fw_name_len < 1 || fw_name_len > 127) {
+        ESP_LOGE(TAG, "Invalid algorithm file name size: %u", alg_name_len);
         return ESP_ERR_INVALID_SIZE;
     }
-
-    char alg_name[128] = { 0 };
-    mpack_node_copy_cstr(alg_name_node, alg_name, sizeof(alg_name));
 
     manifest_cnt += 1;
     manifests[manifest_cnt - 1].fw_name = strdup(fw_name);
     manifests[manifest_cnt - 1].algo_name = strdup(alg_name);
     manifests[manifest_cnt - 1].fw_checksum = fw_checksum;
-    manifests[manifest_cnt - 1].idcode = idcode;
     manifests[manifest_cnt - 1].algo_checksum = alg_checksum;
 
     return ESP_OK;
