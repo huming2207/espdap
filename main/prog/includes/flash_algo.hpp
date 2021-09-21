31cc9793f83209e6cb84840246a858c6c7c32299
Jackson Ming Hu <huming2207@gmail.com>
2021-09-21 23:07:25 +1000

    flash_algo: move to JSON

--- main/prog/includes/flash_algo.hpp
+++ main/prog/includes/flash_algo.hpp
@@ -3,9 +3,11 @@
 #include <cstdint>
 #include <cstddef>
 #include <esp_err.h>
-#include <mpack.h>
+#include <ArduinoJson.hpp>
 
-#define FLASH_ALGO_MAX_MP_SIZE 30720
+#define FLASH_ALGO_MAX_FILE_SIZE 30720
+#define FLASH_ALGO_JSON_DOC_SIZE 40960
+#define FLASH_ALGO_BIN_SIZE 20480
 
 struct sector_info
 {
@@ -18,9 +20,9 @@ class flash_algo
 public:
     flash_algo() = default;
     ~flash_algo();
-    esp_err_t init(const uint8_t *mp_file, size_t len);
-    char *get_algo_name() const;
-    char *get_target_name() const;
+    esp_err_t init(const char *path);
+    const char *get_algo_name() const;
+    const char *get_target_name() const;
     uint8_t *get_algo_bin() const;
     uint32_t get_algo_bin_len() const;
     uint32_t get_ram_size_byte() const;
@@ -37,29 +39,12 @@ public:
     uint32_t get_erased_byte_val() const;
     uint32_t get_program_page_timeout() const;
     uint32_t get_erase_sector_timeout() const;
-    const sector_info &get_sector() const;
+    uint32_t get_sector_size() const;
 
 private:
     static const constexpr char *TAG = "flash_algo";
-    char *algo_name = nullptr;
-    char *target_name = nullptr;
-    uint8_t *algo_bin = nullptr;
+    ArduinoJson::DynamicJsonDocument json_doc = ArduinoJson::DynamicJsonDocument(FLASH_ALGO_JSON_DOC_SIZE);
+    char *algo_buf = new char[FLASH_ALGO_MAX_FILE_SIZE];
+    uint8_t *algo_bin = new uint8_t[FLASH_ALGO_BIN_SIZE];
     uint32_t algo_bin_len = 0;
-    uint32_t ram_size_byte = 0;
-    uint32_t flash_size_byte = 0;
-    uint32_t pc_init = 0;
-    uint32_t pc_uninit = 0;
-    uint32_t pc_program_page = 0;
-    uint32_t pc_erase_sector = 0;
-    uint32_t pc_erase_all = 0;
-    uint32_t data_section_offset = 0;
-    uint32_t flash_start_addr = 0;
-    uint32_t flash_end_addr = 0;
-    uint32_t page_size = 0;
-    uint32_t erased_byte_val = 0;
-    uint32_t program_page_timeout = 0;
-    uint32_t erase_sector_timeout = 0;
-    sector_info sector = {};
-
-    mpack_tree_t mp_tree = {};
 };
