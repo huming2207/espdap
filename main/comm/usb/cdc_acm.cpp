dae2d084495163ce27896a061b38ee5c0c4d8f68
Jackson Ming Hu <huming2207@gmail.com>
2021-10-30 21:26:07 +1100

    cdc_acm: add packet defs

--- main/comm/usb/cdc_acm.cpp
+++ main/comm/usb/cdc_acm.cpp
@@ -1,13 +1,17 @@
 #include <esp_log.h>
+#include <esp_crc.h>
+#include <esp_flash.h>
 
 #include "cdc_acm.hpp"
+
+
 
 esp_err_t cdc_acm::init()
 {
     tinyusb_config_t tusb_cfg = {}; // the configuration using default values
     auto ret = tinyusb_driver_install(&tusb_cfg);
     if (ret != ESP_OK) {
-        ESP_LOGI(TAG, "TinyUSB driver install failed");
+        ESP_LOGE(TAG, "TinyUSB driver install failed");
         return ret;
     }
 
@@ -22,11 +26,205 @@ esp_err_t cdc_acm::init()
 
     ret = ret ?: tusb_cdc_acm_init(&acm_cfg);
 
+    rx_event = xEventGroupCreate();
+    if (rx_event == nullptr) {
+        ESP_LOGE(TAG, "Failed to create Rx event group");
+        return ESP_ERR_NO_MEM;
+    }
+
+    decoded_buf = static_cast<uint8_t *>(heap_caps_malloc(CDC_ACM_DECODED_BUF_LEN, MALLOC_CAP_SPIRAM));
+    if (decoded_buf == nullptr) {
+        ESP_LOGE(TAG, "Failed to allocate SLIP decode buf");
+        return ESP_ERR_NO_MEM;
+    }
+
     return ret;
 }
 
 void cdc_acm::serial_rx_cb(int itf, cdcacm_event_t *event)
 {
     auto &ctx = cdc_acm::instance();
+
+    uint8_t rx_buf[CONFIG_TINYUSB_CDC_RX_BUFSIZE] = { 0 };
+    size_t rx_size = 0;
+    auto ret = tinyusb_cdcacm_read(static_cast<tinyusb_cdcacm_itf_t>(itf), rx_buf, CONFIG_TINYUSB_CDC_RX_BUFSIZE, &rx_size);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "TinyUSB read fail!");
+        return;
+    }
+
+    if (rx_size < 1) return;
+
+    size_t idx = 0;
+    while (idx < rx_size && ctx.curr_rx_len < CDC_ACM_DECODED_BUF_LEN) {
+        if (rx_buf[idx] == SLIP_END) {
+            if (ctx.curr_rx_len > 0) {
+                xEventGroupSetBits(ctx.rx_event, cdc_def::EVT_NEW_PACKET);
+            } else {
+                xEventGroupClearBits(ctx.rx_event, cdc_def::EVT_NEW_PACKET);
+            }
+        } else if (rx_buf[idx] == SLIP_ESC) {
+            idx += 1;
+            if (rx_buf[idx] == SLIP_ESC_END) {
+                ctx.decoded_buf[ctx.curr_rx_len] = SLIP_END;
+            } else if (rx_buf[idx] == SLIP_ESC_ESC) {
+                ctx.decoded_buf[ctx.curr_rx_len] = SLIP_ESC;
+            } else {
+                xEventGroupSetBits(ctx.rx_event, cdc_def::EVT_SLIP_ERROR);
+                ESP_LOGE(TAG, "SLIP decoding detected a corrupted packet");
+                return;
+            }
+        } else {
+            ctx.decoded_buf[ctx.curr_rx_len] = rx_buf[idx];
+        }
+
+        ctx.curr_rx_len += 1;
+    }
+}
+
+[[noreturn]] void cdc_acm::rx_handler_task(void *_ctx)
+{
+    auto &ctx = cdc_acm::instance();
+    while(true) {
+        if (xEventGroupWaitBits(ctx.rx_event, cdc_def::EVT_NEW_PACKET, pdTRUE, pdFALSE, portMAX_DELAY) == pdTRUE) {
+
+        }
+
+
+    }
+}
+
+esp_err_t cdc_acm::send_ack(uint16_t crc, uint32_t timeout_ms)
+{
+    cdc_def::ack_pkt ack = {};
+    ack.crc = crc;
+    ack.type = cdc_def::PKT_ACK;
+    ack.len = 0;
+
+    auto sent_len = tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, (uint8_t *)&ack, sizeof(cdc_def::ack_pkt));
+    if (sent_len < sizeof(cdc_def::ack_pkt)) {
+        ESP_LOGE(TAG, "Failed to send ACK");
+        return ESP_FAIL;
+    } else {
+        return tinyusb_cdcacm_write_flush(TINYUSB_CDC_ACM_0, pdMS_TO_TICKS(timeout_ms));
+    }
+}
+
+esp_err_t cdc_acm::send_dev_info(uint32_t timeout_ms)
+{
+    const char *idf_ver = IDF_VER;
+    const char *dev_model = SI_DEVICE_MODEL;
+    const char *dev_build = SI_DEVICE_BUILD;
+
+    cdc_def::device_info dev_info = {};
+    auto ret = esp_efuse_mac_get_default(dev_info.mac_addr);
+    ret = ret ?: esp_flash_read_unique_chip_id(esp_flash_default_chip, (uint64_t *)dev_info.flash_id);
+    strcpy(dev_info.esp_idf_ver, idf_ver);
+    strcpy(dev_info.dev_build, dev_build);
+    strcpy(dev_info.dev_model, dev_model);
+
+    ret = ret ?: send_pkt(cdc_def::PKT_DEVICE_INFO, (uint8_t *)&dev_info, sizeof(dev_info), timeout_ms);
+
+    return ret;
+}
+
+esp_err_t cdc_acm::send_pkt(cdc_def::pkt_type type, const uint8_t *buf, size_t len, uint32_t timeout_ms)
+{
+    if (buf == nullptr && len > 0) return ESP_ERR_INVALID_ARG;
+
+    cdc_def::header header = {};
+    header.type = type;
+    header.len = len;
+    header.crc = 0; // Set later
+    uint16_t crc = get_crc16_ccitt((uint8_t *)&header, sizeof(header));
+
+    // When packet has no data body, just send header (e.g. ACK)
+    if (buf == nullptr || len < 1) {
+        header.crc = crc;
+        return encode_and_tx((uint8_t *)&header, sizeof(header), nullptr, 0, timeout_ms);
+    }
+
+    crc = get_crc16_ccitt(buf, len, crc);
+    header.crc = crc;
+    return encode_and_tx((uint8_t *)&header, sizeof(header), buf, len, timeout_ms);
+}
+
+esp_err_t cdc_acm::encode_and_tx(const uint8_t *header_buf, size_t header_len,
+                                 const uint8_t *buf, size_t len, uint32_t timeout_ms)
+{
+    if (header_buf == nullptr || header_len < 1) {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    const uint8_t end = SLIP_END;
+
+    if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, &end, 1) < 1) {
+        ESP_LOGE(TAG, "Failed to encode and tx end char");
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    size_t header_idx = 0;
+    while (header_idx < header_len) {
+        if (header_buf[header_idx] == SLIP_END) {
+            if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, slip_esc_end, sizeof(slip_esc_end)) < sizeof(slip_esc_end)) {
+                ESP_LOGE(TAG, "Failed to encode and tx SLIP_END");
+                return ESP_ERR_INVALID_STATE;
+            }
+        } else if (header_buf[header_idx] == SLIP_ESC) {
+            if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, slip_esc_esc, sizeof(slip_esc_esc)) < sizeof(slip_esc_esc)) {
+                ESP_LOGE(TAG, "Failed to encode and tx SLIP_ESC");
+                return ESP_ERR_INVALID_STATE;
+            }
+        } else {
+            if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, &header_buf[header_idx], 1) < 1) {
+                ESP_LOGE(TAG, "Failed to encode and tx data");
+                return ESP_ERR_INVALID_STATE;
+            }
+        }
+
+        header_idx += 1;
+    }
+
+    if (buf == nullptr || len < 1) {
+        size_t payload_idx = 0;
+        while (payload_idx < len) {
+            if (buf[payload_idx] == SLIP_END) {
+                if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, slip_esc_end, sizeof(slip_esc_end)) < sizeof(slip_esc_end)) {
+                    ESP_LOGE(TAG, "Failed to encode and tx SLIP_END");
+                    return ESP_ERR_INVALID_STATE;
+                }
+            } else if (buf[payload_idx] == SLIP_ESC) {
+                if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, slip_esc_esc, sizeof(slip_esc_esc)) < sizeof(slip_esc_esc)) {
+                    ESP_LOGE(TAG, "Failed to encode and tx SLIP_ESC");
+                    return ESP_ERR_INVALID_STATE;
+                }
+            } else {
+                if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, &buf[payload_idx], 1) < 1) {
+                    ESP_LOGE(TAG, "Failed to encode and tx data");
+                    return ESP_ERR_INVALID_STATE;
+                }
+            }
+
+            payload_idx += 1;
+        }
+    }
+
+    if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, &end, 1) < 1) {
+        ESP_LOGE(TAG, "Failed to encode and tx end char");
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    return tinyusb_cdcacm_write_flush(TINYUSB_CDC_ACM_0, pdMS_TO_TICKS(timeout_ms));
+}
+
+uint16_t cdc_acm::get_crc16_ccitt(const uint8_t *buf, size_t len, uint16_t init)
+{
+    //  * CRC-16/CCITT, poly = 0x1021, init = 0x0000, refin = true, refout = true, xorout = 0x0000
+    // *     crc = ~crc16_le((uint16_t)~0x0000, buf, length);
+    if (buf == nullptr || len < 1) {
+        return 0;
+    }
+
+    return ~esp_crc16_le(init, buf, len);
 }
 
--- main/comm/usb/includes/cdc_acm.hpp
+++ main/comm/usb/includes/cdc_acm.hpp
@@ -1,8 +1,64 @@
 #pragma once
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/event_groups.h>
 #include <tinyusb.h>
 #include <tusb_cdc_acm.h>
 #include <esp_err.h>
+
+#define CDC_ACM_DECODED_BUF_LEN     512
+#define SLIP_END            0xc0
+#define SLIP_ESC            0xdb
+#define SLIP_ESC_END        0xdc
+#define SLIP_ESC_ESC        0xdd
+
+#ifndef SI_DEVICE_MODEL
+#define SI_DEVICE_MODEL "SI R&D"
+#endif
+
+#ifndef SI_DEVICE_BUILD
+#define SI_DEVICE_BUILD "0.0.1"
+#endif
+
+namespace cdc_def
+{
+    enum event : uint32_t {
+        EVT_NEW_PACKET = BIT(0),
+        EVT_SLIP_ERROR = BIT(1),
+    };
+
+    enum pkt_type : uint8_t {
+        PKT_ACK = 0,
+        PKT_DEVICE_INFO = 1,
+        PKT_READ_CONFIG = 2,
+        PKT_WRITE_CONFIG = 3,
+        PKT_SET_ALGO_BIN = 4,
+        PKT_GET_ALGO_INFO = 5,
+        PKT_SET_FW_BIN = 6,
+        PKT_SET_FW_INFO = 7,
+    };
+
+    struct __attribute__((packed)) header {
+        pkt_type type;
+        uint8_t len;
+        uint16_t crc;
+    };
+
+    struct __attribute__((packed)) ack_pkt {
+        pkt_type type;
+        uint8_t len;
+        uint16_t crc;
+    };
+
+    struct __attribute__((packed)) device_info {
+        uint8_t mac_addr[6];
+        uint8_t flash_id[8];
+        char esp_idf_ver[32];
+        char dev_model[32];
+        char dev_build[32];
+    };
+}
 
 class cdc_acm
 {
@@ -20,8 +76,22 @@ private:
     cdc_acm() = default;
     esp_err_t init();
     static void serial_rx_cb(int itf, cdcacm_event_t *event);
+    [[noreturn]] static void rx_handler_task(void *ctx);
+    static esp_err_t send_pkt(cdc_def::pkt_type type, const uint8_t *buf, size_t len, uint32_t timeout_ms = portMAX_DELAY);
+    static esp_err_t encode_and_tx(const uint8_t *header_buf, size_t header_len, const uint8_t *buf, size_t len, uint32_t timeout_ms = portMAX_DELAY);
+    static inline uint16_t get_crc16_ccitt(const uint8_t *buf, size_t len, uint16_t init = 0xffff);
+
+public:
+    static esp_err_t send_ack(uint16_t crc = 0, uint32_t timeout_ms = portMAX_DELAY);
+    static esp_err_t send_dev_info(uint32_t timeout_ms = portMAX_DELAY);
 
 private:
     static const constexpr char *TAG = "cdc_acm";
+    static const constexpr uint8_t slip_esc_end[] = { SLIP_ESC, SLIP_ESC_END };
+    static const constexpr uint8_t slip_esc_esc[] = { SLIP_ESC, SLIP_ESC_ESC };
+    EventGroupHandle_t rx_event = nullptr;
+    volatile bool busy_decoding = false;
+    volatile size_t curr_rx_len = 0;
+    uint8_t *decoded_buf = nullptr;
 };
 
