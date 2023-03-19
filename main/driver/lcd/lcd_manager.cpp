7fc275900f45cf14542f00e9c171b92dc7709946
Jackson Ming Hu <huming2207@gmail.com>
2023-03-19 22:58:03 +1100

    lcd: implement all other UIs

--- main/driver/lcd/lcd_manager.cpp
+++ main/driver/lcd/lcd_manager.cpp
@@ -8,7 +8,7 @@ esp_err_t lcd_manager::init()
 
     if (ret == ESP_OK) {
         curr_state = lcd::STATE_SPLASH;
-        return display_splash();
+        return display_done();
     }
 
     return ret;
@@ -56,13 +56,29 @@ esp_err_t lcd_manager::display_detect()
 
     lv_obj_t *top_bar = nullptr;
     lv_obj_t *bottom_bar = nullptr;
-    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_black(), lv_color_hex(0x006065)); // Dark cyan
+    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_hex(0x005f56), lv_color_hex(0xb26500)); // Dark cyan
+
+    auto *top_text = lv_label_create(top_bar);
+    lv_obj_align(top_text, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_align(top_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text(top_text, "Detecting");
+    lv_obj_set_style_text_font(top_text, &lv_font_montserrat_32, 0);
+    lv_obj_set_style_text_color(top_text, lv_color_white(), 0);
+
+    auto *bottom_text = lv_label_create(bottom_bar);
+    lv_obj_align(bottom_text, LV_ALIGN_CENTER, 0,16);
+    lv_obj_set_style_text_align(bottom_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_set_size(bottom_text, 200, 100);
+    lv_label_set_long_mode(bottom_text, LV_LABEL_LONG_WRAP);
+    lv_label_set_text(bottom_text, "Please connect pogo pin to target product");
+    lv_obj_set_style_text_font(bottom_text, &lv_font_montserrat_20, 0);
+    lv_obj_set_style_text_color(bottom_text, lv_color_black(), 0);
 
     lvgl_give_lock();
     return ret;
 }
 
-esp_err_t lcd_manager::display_erase(bool is_chip_erase)
+esp_err_t lcd_manager::display_erase()
 {
     auto ret = clear_display();
     if (ret != ESP_OK) {
@@ -73,7 +89,24 @@ esp_err_t lcd_manager::display_erase(bool is_chip_erase)
 
     lv_obj_t *top_bar = nullptr;
     lv_obj_t *bottom_bar = nullptr;
-    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_black(), lv_color_hex(0x01579b)); // Dark blue
+    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_hex(0x01579b), lv_color_hex(0x631976)); // Dark blue
+
+    auto *top_text = lv_label_create(top_bar);
+    lv_obj_align(top_text, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_align(top_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text(top_text, "Erasing");
+    lv_obj_set_style_text_font(top_text, &lv_font_montserrat_32, 0);
+    lv_obj_set_style_text_color(top_text, lv_color_white(), 0);
+
+    auto *bottom_text = lv_label_create(bottom_bar);
+    lv_obj_align(bottom_text, LV_ALIGN_CENTER, 0,10);
+    lv_obj_set_style_text_align(bottom_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_set_size(bottom_text, 200, 100);
+    lv_label_set_long_mode(bottom_text, LV_LABEL_LONG_WRAP);
+    lv_label_set_text(bottom_text, "Erasing target\n\nDO NOT DISCONNECT\n");
+
+    lv_obj_set_style_text_font(bottom_text, &lv_font_montserrat_20, 0);
+    lv_obj_set_style_text_color(bottom_text, lv_color_white(), 0);
 
     lvgl_give_lock();
     return ret;
@@ -90,7 +123,22 @@ esp_err_t lcd_manager::display_program(uint8_t percentage)
 
     lv_obj_t *top_bar = nullptr;
     lv_obj_t *bottom_bar = nullptr;
-    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_black(), lv_color_hex(0xffab00)); // Orange
+    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_hex(0xffab00), lv_color_white()); // Orange
+
+    auto *top_text = lv_label_create(top_bar);
+    lv_obj_align(top_text, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_align(top_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text(top_text, "Programming");
+    lv_obj_set_style_text_font(top_text, &lv_font_montserrat_28, 0);
+    lv_obj_set_style_text_color(top_text, lv_color_white(), 0);
+
+    auto *bottom_progress = lv_bar_create(bottom_bar);
+    lv_obj_align(bottom_progress, LV_ALIGN_TOP_LEFT, 0,0);
+    lv_obj_set_style_radius(bottom_progress, 0, 0);
+    lv_obj_set_style_radius(bottom_progress, 0, LV_PART_INDICATOR);
+    lv_obj_set_style_text_align(bottom_progress, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_set_size(bottom_progress, 240, 120);
+    lv_bar_set_value(bottom_progress, percentage, LV_ANIM_OFF);
 
     lvgl_give_lock();
     return ret;
@@ -109,6 +157,26 @@ esp_err_t lcd_manager::display_error(const char *err_heading, const char *err_ms
     lv_obj_t *bottom_bar = nullptr;
     ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_black(), lv_color_hex(0xff0000)); // Red
 
+    auto *top_text = lv_label_create(top_bar);
+    lv_obj_align(top_text, LV_ALIGN_CENTER, 0, -15);
+    lv_obj_set_style_text_align(top_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text_fmt(top_text, "Error\n%s", err_heading == nullptr ? "Unknown" : err_heading);
+    lv_obj_set_style_text_font(top_text, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_color(top_text, lv_color_white(), 0);
+
+    auto *bottom_text = lv_label_create(bottom_bar);
+    lv_obj_align(bottom_text, LV_ALIGN_TOP_LEFT, 10, 2);
+    lv_obj_set_style_text_align(bottom_text, LV_TEXT_ALIGN_LEFT, 0);
+
+    if (err_msg != nullptr) {
+        lv_label_set_text(bottom_text, err_msg);
+    } else {
+        lv_label_set_text(bottom_text, "No error message");
+    }
+
+    lv_obj_set_style_text_font(bottom_text, &lv_font_montserrat_12, 0);
+    lv_obj_set_style_text_color(bottom_text, lv_color_white(), 0);
+
     lvgl_give_lock();
     return ret;
 }
@@ -124,7 +192,27 @@ esp_err_t lcd_manager::display_verify(const char *verify_msg)
 
     lv_obj_t *top_bar = nullptr;
     lv_obj_t *bottom_bar = nullptr;
-    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_black(), lv_color_hex(0x512da8)); // Red
+    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_hex(0x512da8), lv_color_white()); // Red
+
+    auto *top_text = lv_label_create(top_bar);
+    lv_obj_align(top_text, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_align(top_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text_fmt(top_text, "Verifying");
+    lv_obj_set_style_text_font(top_text, &lv_font_montserrat_28, 0);
+    lv_obj_set_style_text_color(top_text, lv_color_white(), 0);
+
+    auto *bottom_text = lv_label_create(bottom_bar);
+    lv_obj_align(bottom_text, LV_ALIGN_TOP_LEFT, 10, 6);
+    lv_obj_set_style_text_align(bottom_text, LV_TEXT_ALIGN_LEFT, 0);
+
+    if (verify_msg != nullptr) {
+        lv_label_set_text(bottom_text, verify_msg);
+    } else {
+        lv_label_set_text(bottom_text, "Please wait..");
+    }
+
+    lv_obj_set_style_text_font(bottom_text, &lv_font_montserrat_18, 0);
+    lv_obj_set_style_text_color(bottom_text, lv_color_black(), 0);
 
     lvgl_give_lock();
     return ret;
@@ -141,7 +229,23 @@ esp_err_t lcd_manager::display_done()
 
     lv_obj_t *top_bar = nullptr;
     lv_obj_t *bottom_bar = nullptr;
-    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_black(), lv_color_hex(0x1b5e20)); // Red
+    ret = ret ?: draw_two_bars(&top_bar, &bottom_bar, lv_color_hex(0x1b5e20), lv_color_white()); // Red
+
+    auto *top_text = lv_label_create(top_bar);
+    lv_obj_align(top_text, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_align(top_text, LV_TEXT_ALIGN_CENTER, 0);
+    lv_label_set_text_fmt(top_text, "Done!");
+    lv_obj_set_style_text_font(top_text, &lv_font_montserrat_28, 0);
+    lv_obj_set_style_text_color(top_text, lv_color_white(), 0);
+
+    auto *bottom_text = lv_label_create(bottom_bar);
+    lv_obj_align(bottom_text, LV_ALIGN_CENTER, 0, 6);
+    lv_obj_set_style_text_align(bottom_text, LV_ALIGN_CENTER, 0);
+    lv_obj_set_size(bottom_text, 200, 100);
+    lv_label_set_text(bottom_text, "Please connect to the next product and press the button to continue...");
+    lv_obj_set_style_text_font(bottom_text, &lv_font_montserrat_18, 0);
+    lv_obj_set_style_text_color(bottom_text, lv_color_black(), 0);
+
 
     lvgl_give_lock();
     return ret;
