c6dab2460ce6667db5f5c30c8aeff1b26ea89d7e
Jackson Ming Hu <huming2207@gmail.com>
2024-05-23 22:10:15 +1000

    rpc_packet: implement msgpack serialising

--- main/comm/rpc_packet.hpp
+++ main/comm/rpc_packet.hpp
@@ -1,38 +1,126 @@
 #pragma once
 
+#include <cstdint>
+#include <ArduinoJson.hpp>
+#include <psram_json_allocator.hpp>
+
 namespace rpc
 {
-    struct __attribute__((packed)) prog_event
+    enum event_type : uint8_t
     {
-        uint32_t project_id;
-        uint32_t addr;
-        uint32_t len;
-        char target_sn[32];
+        EVENT_PROGRAM = 0x10,
+        EVENT_TEST_INTERNAL = 0x20,
+        EVENT_TEST_EXTERNAL = 0x21, // Do we need this?
+        EVENT_ERASE = 0x30,
+        EVENT_REPAIR = 0x40,
+        EVENT_DISPOSE = 0x50,
     };
 
-    struct __attribute__((packed)) self_test_event
+    struct base_event
     {
-        uint32_t test_id;
-        uint32_t return_num;
-        char test_name[32];
-        char target_sn[32];
+    protected:
+        PsRamAllocator allocator;
+        ArduinoJson::JsonDocument document;
+
+    public:
+        event_type type;
+        explicit base_event(event_type _type) : allocator{}, document(&allocator), type(_type) {}
+        virtual size_t serialize(uint8_t *buf_out, size_t buf_size) = 0;
     };
 
-    struct __attribute__((packed)) erase_event
+    struct prog_event : public base_event
     {
-        uint32_t addr;
-        uint32_t len;
-        char target_sn[32];
+        prog_event() : base_event(EVENT_PROGRAM) {}
+
+        size_t serialize(uint8_t *buf_out, size_t buf_size) override
+        {
+            document.clear();
+            document["type"] = type;
+            document["projID"] = project_id;
+            document["addr"] = addr;
+            document["len"] = len;
+            document["sn"] = ArduinoJson::MsgPackBinary(target_sn, sizeof(target_sn));
+
+            return ArduinoJson::serializeMsgPack(document, (void *)buf_out, buf_size);
+        }
+
+        uint32_t project_id{};
+        uint32_t addr{};
+        uint32_t len{};
+        uint8_t target_sn[32]{};
     };
 
-    struct __attribute__((packed)) repair_event
+    struct self_test_event : public base_event
     {
-        char target_sn[32];
+        self_test_event() : base_event(EVENT_TEST_INTERNAL) {}
+
+        size_t serialize(uint8_t *buf_out, size_t buf_size) override
+        {
+            document.clear();
+            document["type"] = type;
+            document["testID"] = test_id;
+            document["ret"] = return_num;
+            document["name"] = test_name;
+            document["sn"] = ArduinoJson::MsgPackBinary(target_sn, sizeof(target_sn));
+
+            return ArduinoJson::serializeMsgPack(document, (void *)buf_out, buf_size);
+        }
+
+        uint32_t test_id{};
+        uint32_t return_num{};
+        char test_name[32]{};
+        uint8_t target_sn[32]{};
     };
 
-
-    struct __attribute__((packed)) dispose_event
+    struct erase_event : public base_event
     {
-        char target_sn[32];
+        erase_event() : base_event(EVENT_ERASE) {}
+
+        size_t serialize(uint8_t *buf_out, size_t buf_size) override
+        {
+            document.clear();
+            document["type"] = type;
+            document["addr"] = addr;
+            document["len"] = len;
+            document["sn"] = ArduinoJson::MsgPackBinary(target_sn, sizeof(target_sn));
+
+            return ArduinoJson::serializeMsgPack(document, (void *)buf_out, buf_size);
+        }
+
+        uint32_t addr{};
+        uint32_t len{};
+        uint8_t target_sn[32]{};
+    };
+
+    struct repair_event : public base_event
+    {
+        repair_event() : base_event(EVENT_REPAIR) {}
+
+        size_t serialize(uint8_t *buf_out, size_t buf_size) override
+        {
+            document.clear();
+            document["type"] = type;
+            document["sn"] = ArduinoJson::MsgPackBinary(target_sn, sizeof(target_sn));
+
+            return ArduinoJson::serializeMsgPack(document, (void *)buf_out, buf_size);
+        }
+
+        uint8_t target_sn[32]{};
+    };
+
+    struct dispose_event : public base_event
+    {
+        dispose_event() : base_event(EVENT_DISPOSE) {}
+
+        size_t serialize(uint8_t *buf_out, size_t buf_size) override
+        {
+            document.clear();
+            document["type"] = type;
+            document["sn"] = ArduinoJson::MsgPackBinary(target_sn, sizeof(target_sn));
+
+            return ArduinoJson::serializeMsgPack(document, (void *)buf_out, buf_size);
+        }
+
+        uint8_t target_sn[32]{};
     };
 }
