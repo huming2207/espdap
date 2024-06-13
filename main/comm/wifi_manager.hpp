c44de051bfaf7f278ca2d4d977401019c646a888
Jackson Ming Hu <huming2207@gmail.com>
2024-06-13 22:55:56 +1000

    wifi: fix compile

--- main/comm/wifi_manager.hpp
+++ main/comm/wifi_manager.hpp
@@ -1,29 +1,21 @@
 #pragma once
 
+#include <esp_netif.h>
+#include <esp_event.h>
 #include <functional>
 
 class wifi_manager
 {
 public:
-    static wifi_manager *instance()
-    {
-        static wifi_manager _instance;
-        return &_instance;
-    }
-
-    void operator=(wifi_manager const &) = delete;
-    wifi_manager(wifi_manager const &) = delete;
-
-public:
     esp_err_t init();
-    static void event_handler(void *_ctx, esp_event_base_t evt_base, int32_t evt_id, void *evt_data);
     void set_lost_ip_cb(const std::function<void()> &cb);
     void set_got_ip_cb(const std::function<void(esp_netif_ip_info_t *)> &cb);
 
 private:
-    wifi_manager() = default;
     std::function<void()> lost_ip_cb = {};
     std::function<void(esp_netif_ip_info_t *)> got_ip_cb = {};
+
+    static void event_handler(void *_ctx, esp_event_base_t evt_base, int32_t evt_id, void *evt_data);
 
 private:
     static const constexpr char TAG[] = "wifi_mgr";
